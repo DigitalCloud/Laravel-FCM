e7a00de575bd02745f5d010058d3686e529c4734
Amroo Salout <a.salout@dce.sa>
2020-12-02 08:41:12 +0200

    support laravel 8

--- src/FCMManager.php
+++ src/FCMManager.php
@@ -9,12 +9,12 @@ class FCMManager extends Manager
 {
     public function getDefaultDriver()
     {
-        return $this->app[ 'config' ][ 'fcm.driver' ];
+        return config('fcm.driver');//$this->app[ 'config' ][ 'fcm.driver' ];
     }
 
     protected function createHttpDriver()
     {
-        $config = $this->app[ 'config' ]->get('fcm.http', []);
+        $config = config('fcm.http');//$this->app[ 'config' ]->get('fcm.http', []);
 
         return new Client(['timeout' => $config[ 'timeout' ]]);
     }
